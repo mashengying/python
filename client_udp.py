8963640040a8b79a62c1e8e7a4706bf3b7126118
wenjun <469509908@qq.com>
2022-05-09 23:16:33 +0800

    update

--- client_udp.py
+++ client_udp.py
@@ -1,9 +1,11 @@
 from socket import *
-serverName="localhost"
-serverPort=12000
+serverName="81.70.242.195"
+serverPort=5800
 clientSocket=socket(AF_INET,SOCK_DGRAM)
-message=input('input letters:')
-clientSocket.sendto(message.encode(),(serverName,serverPort))
-newMessage,serverAddress=clientSocket.recvfrom(2048)
-print(newMessage)
-print(serverAddress)
+
+while True:
+    message=input('input letters:')
+    clientSocket.sendto(message.encode(),(serverName,serverPort))
+    newMessage,serverAddress=clientSocket.recvfrom(2048)
+    print(newMessage)
+    print(serverAddress)
