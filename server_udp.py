15c4b68532cef2f4e6532f94e6500c38b51d6fea
mashengying <31642956+mashengying@users.noreply.github.com>
2022-05-09 22:20:02 +0800

    add

---
+++ server_udp.py
@@ -0,0 +1,10 @@
+from email import message
+from socket import *
+serverName="localhost"
+serverPort=12000
+clientSocket=socket(AF_INET,SOCK_DGRAM)
+message=input('input letters:')
+clientSocket.sendto(message.encode(),(serverName,serverPort))
+newMessage,serverAddress=clientSocket.recvfrom(2048)
+print(newMessage)
+print('serverAddress:'+serverAddress)
